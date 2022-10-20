f3c01abbe1ade5b3fafa9193b4156954d694128e
Jazz <seunghohan25@gmail.com>
2022-10-20 20:43:41 +0900

    Time: 72 ms (34.23%), Space: 24.9 MB (84.55%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -9,16 +9,16 @@ public:
         // }
         
         // 2
-        // if (nums.empty() || (k %= nums.size()) == 0) return;
-        // int n = nums.size();
-        // reverse(nums.begin(), nums.begin() + n - k);
-        // reverse(nums.begin() + n - k, nums.end());
-        // reverse(nums.begin(), nums.end());
+        if (nums.empty() || (k %= nums.size()) == 0) return;
+        int n = nums.size();
+        reverse(nums.begin(), nums.begin() + n - k);
+        reverse(nums.begin() + n - k, nums.end());
+        reverse(nums.begin(), nums.end());
         
         // 3
-        vector<int> t = nums;
-        for (int i = 0; i < nums.size(); ++i) {
-            nums[(i + k) % nums.size()] = t[i];
-        }
+        // vector<int> t = nums;
+        // for (int i = 0; i < nums.size(); ++i) {
+        //     nums[(i + k) % nums.size()] = t[i];
+        // }
     }
 };
