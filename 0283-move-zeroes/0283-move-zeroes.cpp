db790bd6d39384191c16abf6c22d541e8346e1bc
Jazz <seunghohan25@gmail.com>
2022-10-20 21:17:19 +0900

    Time: 113 ms (11.61%), Space: 19.3 MB (57.74%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        stable_partition(nums.begin(), nums.end(), [](int n) { return n != 0; });
+        // 1
+        // stable_partition(nums.begin(), nums.end(), [](int n) { return n != 0; });
+        
+        //2
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                count++;
+                // deleting the element from vector
+                nums.erase(nums.begin() + i);
+                i--;
+            }
+        }
+
+        for (int i = 0; i < count; i++) {
+            // inserting the zero into vector
+            nums.push_back(0);
+        }
+        
     }
 };
