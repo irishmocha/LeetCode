955f167c8a3f9b2aaa92e4342a12d45f3daf0687
Jazz <seunghohan25@gmail.com>
2022-10-20 21:20:18 +0900

    Time: 16 ms (99.29%), Space: 19.3 MB (57.74%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // 1
-        stable_partition(nums.begin(), nums.end(), [](int n) { return n != 0; });
+        /* 1
+        Runtime: 66 ms, faster than 21.18% of C++ online submissions for Move Zeroes.
+        Memory Usage: 19.5 MB, less than 8.20% of C++ online submissions for Move Zeroes.
+        */
+        // stable_partition(nums.begin(), nums.end(), [](int n) { return n != 0; });
         
         /* 2.
         Runtime: 113 ms, faster than 11.61% of C++ online submissions for Move Zeroes.
@@ -22,6 +25,16 @@ public:
 //             // inserting the zero into vector
 //             nums.push_back(0);
 //         }
+        
+        /* 3.
+        
+        */
+        for (int i = 0, j = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[j], nums[i]); // Partitioning the array
+                j++;
+            }
+        }
         
     }
 };
