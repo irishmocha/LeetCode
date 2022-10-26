2c0e38a9186d9d133e6bd25d3389d86179b11b72
Jazz <seunghohan25@gmail.com>
2022-10-26 22:26:52 +0900

    Time: 3 ms (48.42%), Space: 6.7 MB (17.83%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,46 +1,31 @@
 class Solution {
+private:
+    map<char, string> m = {
+        {'2', "abc"}, {'3', "def"}, {'4', "ghi"},
+        {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"},
+        {'8', "tuv"}, {'9', "wxyz"}
+    };
+
+    vector<string> result;
+
 public:
-    char letters[3][10] = {{'a', 'a', 'a', 'd', 'g', 'j', 'm', 'p', 't', 'w'},
-                           {'b', 'b', 'b', 'e', 'h', 'k', 'n', 'q', 'u', 'x'},
-                           {'c', 'c', 'c', 'f', 'i', 'l', 'o', 'r', 'v', 'y'}};
-    void helper(const string &digits, int index, string &s, vector<string> &result) {
-        if (index >= s.size()) {
-            return ;
-        }
-        int digit = digits[index] - '0';
-        if (index + 1 == s.size()) {
-            for (int i = 0; i < 3; ++i) {
-                s[index] = letters[i][digit];
-                result.push_back(s);
-            }
-            if (digit == 7) {
-                s[index] = 's';
-                result.push_back(s);
-            } else if (digit == 9) {
-                s[index] = 'z';
-                result.push_back(s);
-            }
-            return ;
-        }
-        for (int i = 0; i < 3; ++i) {
-            s[index] = letters[i][digit];
-            helper(digits, index + 1, s, result);
-        }
-        if (digit == 7) {
-            s[index] = 's';
-            helper(digits, index + 1, s, result);
-        } else if (digit == 9) {
-            s[index] = 'z';
-            helper(digits, index + 1, s, result);
-        }
-        return ;
-    }
     vector<string> letterCombinations(string digits) {
-        if (digits.size() == 0)
-            return vector<string>();
-        string s = digits;
-        vector<string> result;
-        helper(digits, 0, s, result);
+        if(digits.size() != 0){
+            generateCombination("", digits, 0);
+        }
+
         return result;
+    }
+
+    void generateCombination(string current, string digits, int index) {
+        if(index == digits.size()){
+            result.push_back(current);
+        } else {
+            char currentDigit = digits[index];
+            string mapping = m[currentDigit];
+            for(int i = 0; i < mapping.size(); i++){
+                generateCombination(current + mapping[i], digits, index+1);
+            }
+        }
     }
 };
