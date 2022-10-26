31c3740d0892ba1ca79289855ab2d15c46570e72
Jazz <seunghohan25@gmail.com>
2022-10-26 22:34:45 +0900

    Time: 3 ms (79.51%), Space: 7.5 MB (55.01%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -10,10 +10,10 @@ class Solution {
 public:
     ListNode *swapPairs(ListNode *head) {
         if(head==NULL || head->next==NULL) return head; 
-        ListNode *start=head->next; 
-        ListNode *next=start->next;
-        start->next=head;
-        head->next=swapPairs(next);
+        ListNode *start = head->next; 
+        ListNode *next = start->next;
+        start->next = head;
+        head->next = swapPairs(next);
         return start; 
     }
 };
