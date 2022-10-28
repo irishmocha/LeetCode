285fccd7623b1caf9d87630f2e4af26487571f66
Jazz <seunghohan25@gmail.com>
2022-10-28 22:10:47 +0900

    Time: 67 ms (62.95%), Space: 89.8 MB (39.15%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,9 +3,7 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         nums1.insert(nums1.end(), nums2.begin(), nums2.end());
         sort(nums1.begin(), nums1.end());
-        // cout << nums1.size() << ", " << nums1.size() / 2 << endl;
         if (nums1.size() % 2 != 0) {
-            // cout << "!!!" << endl;
             return (double) nums1[nums1.size() / 2];
         }
         else {
