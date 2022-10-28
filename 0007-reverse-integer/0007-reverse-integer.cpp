f53686096ca05c8e26f05a60921ec2289bdb5e72
Jazz <seunghohan25@gmail.com>
2022-10-28 22:34:04 +0900

    Time: 0 ms (100.00%), Space: 6 MB (29.43%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,11 +3,11 @@ public:
     int reverse(int x) {
         string s = to_string(x);
         ::reverse(s.begin(), s.end());
-        
-        long result = stol(s);
-        if(result<= -2147483648 || result >=  2147483647)
+        if (stol(s)<= -2147483648 || stol(s) >=  2147483647) {
             return 0;
-        
-        return x > 0 ? stoi(s) : stoi(s) * -1;
+        }
+        else {
+            return x > 0 ? stoi(s) : stoi(s) * -1;
+        }
     }
 };
