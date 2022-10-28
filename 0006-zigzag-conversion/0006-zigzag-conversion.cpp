c65c1055f20c6bea951a115389a94c38ab731a2d
Jazz <seunghohan25@gmail.com>
2022-10-28 22:22:37 +0900

    Time: 25 ms (50.13%), Space: 8.3 MB (82.68%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows == 1)    return s;
+        
+        int len = s.length();
+        string res = "";
+        
+        int tmp = 0;
+        int circle = numRows - 1, inc = 2 * circle;
+        while(tmp < len) {
+            res += s[tmp];
+            tmp += inc;
+        }
+        
+        for(int i = 1; i < circle; i++) {
+            tmp = i;
+            int sum = inc;
+            while(tmp < len) {
+                res += s[tmp];
+                if(sum - tmp < len)
+                    res += s[sum - tmp];
+                tmp += inc;
+                sum += 2 * inc;
+            }
+        }
+        
+        tmp = circle;
+        while(tmp < len) {
+            res += s[tmp];
+            tmp += inc;
+        }
+        
+        return res;
+    }
+};
