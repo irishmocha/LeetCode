ee892e5c5a91f026db9c798dcabaf78657f847ec
Jazz <seunghohan25@gmail.com>
2022-11-03 23:03:56 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (30.57%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> answer;
         for (int i = 0; i < numRows; ++i) {
-            answer.push_back(vector<int>(i + 1,1));
+            answer.push_back(vector<int>(i + 1, 1));
             for (int j = 1; j < i; ++j) {
                 answer[i][j] = answer[i-1][j-1] + answer[i-1][j];
             }
