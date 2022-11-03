c0ed43f203c2b3c46e0b907a8f34ede0d8ea6261
Jazz <seunghohan25@gmail.com>
2022-11-03 22:49:43 +0900

    Time: 497 ms (56.49%), Space: 114.2 MB (68.71%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,38 +11,16 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if (head==NULL || head->next == NULL) {
-            return true;
-        }
-        
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast->next != NULL && fast->next->next != NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        slow->next = reverseList(slow->next);
-        slow = slow->next;
-        while (slow != NULL) {
-            if (head->val != slow->val) {
-                return false;
-            }
-            head = head->next;
-            slow = slow->next;
-        }
+        ListNode *slow = head, *fast = head, *prev, *temp;
+        while (fast && fast->next)
+            slow = slow->next, fast = fast->next->next;
+        prev = slow, slow = slow->next, prev->next = NULL;
+        while (slow)
+            temp = slow->next, slow->next = prev, prev = slow, slow = temp;
+        fast = head, slow = prev;
+        while (slow)
+            if (fast->val != slow->val) return false;
+            else fast = fast->next, slow = slow->next;
         return true;
-    }
-    
-    ListNode* reverseList(ListNode* head) {
-        ListNode* pre = NULL;
-        ListNode* next = NULL;
-        while (head != NULL) {
-            next = head->next;
-            head->next = pre;
-            pre = head;
-            head = next;
-        }
-        
-        return pre;
     }
 };
