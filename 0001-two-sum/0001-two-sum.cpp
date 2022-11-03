39a3e2c7121043c2a35e217085fa8f93d61d0e85
Jazz <seunghohan25@gmail.com>
2022-11-03 21:03:17 +0900

    Time: 26 ms (67.67%), Space: 10.9 MB (45.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,15 +3,25 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> answer;
         
+//         for (int i = 0; i < nums.size(); ++i) {
+//             for (int j = i + 1; j < nums.size(); ++j) {
+//                 if (nums[i] + nums[j] == target) {
+//                     answer.push_back(i);
+//                     answer.push_back(j);
+//                     break;
+//                 }
+//             }
+//         }
+        
+        unordered_map<int, int> u;
         for (int i = 0; i < nums.size(); ++i) {
-            for (int j = i + 1; j < nums.size(); ++j) {
-                if (nums[i] + nums[j] == target) {
-                    answer.push_back(i);
-                    answer.push_back(j);
-                    break;
-                }
+            int complement = target - nums[i];
+            if (u.find(complement) != u.end()) {
+                return vector<int>{u[complement], i};
             }
+            u[nums[i]] = i;
         }
+        
         
         return answer;
     }
