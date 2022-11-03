d7cb388cf8e3280fd6fccee3cc3aca12ca6635a2
Jazz <seunghohan25@gmail.com>
2022-11-03 23:14:45 +0900

    Time: 0 ms (100.00%), Space: 8.6 MB (37.29%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> runningSum(vector<int>& nums) {
         vector<int> answer;
         // int acc = 0;
-        for (int i = 0, acc = 0; i < nums.size(); ++i) {
+        for (int i = 0, acc = 0; i < nums.size(); acc += nums[i], ++i) {
             answer.push_back(acc + nums[i]);
-            acc += nums[i];
+            // acc += nums[i];
         }
         return answer;
     }
