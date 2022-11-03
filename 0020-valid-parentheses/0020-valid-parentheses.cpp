413fe91692afa5b559a7556e22f4728f89114de9
Jazz <seunghohan25@gmail.com>
2022-11-03 21:41:12 +0900

    Time: 4 ms (34.21%), Space: 6.4 MB (50.70%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                // cout << "push " << s[i] << endl;
+                stk.push(s[i]);
+            }
+            else {
+                
+                if (!stk.empty()) {
+                    if (stk.top() == '(' && s[i] == ')') stk.pop();
+                    else if (stk.top() == '{' && s[i] == '}') stk.pop();
+                    else if (stk.top() == '[' && s[i] == ']') stk.pop();
+                    else stk.push(s[i]);
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return stk.empty();
+    }
+};
