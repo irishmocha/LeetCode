0b2ccf7c83a5510fee9982d33570b656f8c02932
Jazz <seunghohan25@gmail.com>
2022-11-03 22:37:15 +0900

    Time: 10 ms (45.58%), Space: 9.3 MB (53.60%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans = strs[0];
+        string answer = strs[0];
         
         for (int i = 1; i < strs.size(); i++) {
             int j;
             for (j = 0; j < strs[i].size(); j++) {
-                if (ans[j] != strs[i][j])
+                if (answer[j] != strs[i][j])
                     break;
             }
-            ans = ans.substr(0, j);
-            if (ans == "")
-                return ans;
+            answer = answer.substr(0, j);
+            if (answer == "")
+                return answer;
         }
         
-        return ans;
+        return answer;
     }
 };
