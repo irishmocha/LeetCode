961a2be9b8e896407e214cf3599aa09a2b396a38
Jazz <seunghohan25@gmail.com>
2022-11-06 15:31:07 +0900

    Time: 26 ms (90.65%), Space: 19.3 MB (13.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,41 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        /* 1
-        Runtime: 66 ms, faster than 21.18% of C++ online submissions for Move Zeroes.
-        Memory Usage: 19.5 MB, less than 8.20% of C++ online submissions for Move Zeroes.
-        */
-        // stable_partition(nums.begin(), nums.end(), [](int n) { return n != 0; });
-        
-        /* 2.
-        Runtime: 113 ms, faster than 11.61% of C++ online submissions for Move Zeroes.
-        Memory Usage: 19.3 MB, less than 57.74% of C++ online submissions for Move Zeroes.
-        */
-//         int count = 0;
-//         for (int i = 0; i < nums.size(); i++) {
-//             if (nums[i] == 0) {
-//                 count++;
-//                 // deleting the element from vector
-//                 nums.erase(nums.begin() + i);
-//                 i--;
-//             }
-//         }
-
-//         for (int i = 0; i < count; i++) {
-//             // inserting the zero into vector
-//             nums.push_back(0);
-//         }
-        
-        /* 3.
-        Runtime: 16 ms, faster than 99.29% of C++ online submissions for Move Zeroes.
-        Memory Usage: 19.3 MB, less than 57.74% of C++ online submissions for Move Zeroes.
-        */
-        for (int i = 0, j = 0; i < nums.size(); i++) {
+        int lastNonZeroFoundAt = 0;
+        for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
-                swap(nums[j], nums[i]); // Partitioning the array
-                j++;
+                nums[lastNonZeroFoundAt++] = nums[i];
             }
         }
         
+        for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
     }
 };
