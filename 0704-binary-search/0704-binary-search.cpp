94696b4f31128824275cf2aafa849c7c53c9d47a
Jazz <seunghohan25@gmail.com>
2022-11-06 13:46:27 +0900

    Time: 81 ms (59.61%), Space: 27.6 MB (12.33%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int index;
         int lower = 0;
         int upper = nums.size() - 1;
         
+        int index;
         while (lower <= upper) {
             index = (lower + upper) / 2;
-            if (nums[index] == target) return index;
-            if (nums[index] < target) lower = index + 1;
-            else upper = index - 1;
+            if (nums[index] == target) {
+                return index;
+            }
+            
+            if (nums[index] < target) {
+                lower = index + 1;
+            }
+            else {
+                upper = index - 1;
+            }
             
         }
+        
         return -1;
     }
 };
