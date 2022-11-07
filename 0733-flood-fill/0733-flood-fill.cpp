926942a9a32173ac9ee2948a86c100aff04b7c21
Jazz <seunghohan25@gmail.com>
2022-11-07 20:41:08 +0900

    Time: 17 ms (60.65%), Space: 14.2 MB (27.19%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -32,16 +32,27 @@ public:
     }
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if (image[sr][sc] == color) return image;
-        
-        m = image.size();
-        n = image[0].size();
-        
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        
-        dfs(image, sr, sc, color, image[sr][sc], visited);
-        
-        
+        int m = image.size();
+        int n = image[0].size();
+        queue<pair<int, int>> q;
+        q.push(make_pair(sr, sc));
+        int startColor = image[sr][sc];
+        if (startColor == color) return image;
+        int e[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        image[sr][sc] = color;
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for (int k = 0; k < 4; k++) {
+                int x = r + e[k][0];
+                int y = c + e[k][1];
+                if (x >= 0 && y >= 0 && x < m && y < n && image[x][y] == startColor) {
+                    image[x][y] = color;
+                    q.push(make_pair(x, y));
+                }
+            }
+        }
         return image;
     }
 };
