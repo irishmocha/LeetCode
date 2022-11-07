bd31c6e4903a6347bd817a5422caab1cf7b98ed3
Jazz <seunghohan25@gmail.com>
2022-11-07 23:11:13 +0900

    Time: 194 ms (30.42%), Space: 30.1 MB (72.37%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        if (mat.size() == 0) {
+            return mat;
+        }
+        
+        int rows = mat.size();
+        int cols = mat[0].size();
+        vector<vector<int>> dist(rows, vector<int> (cols, INT_MAX));
+        
+        queue<pair<int, int>> q;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                    q.push({ i, j }); //Put all 0s in the queue.
+                }
+            }
+        }
+
+        int dir[4][2] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+        while (!q.empty()) {
+            pair<int, int> curr = q.front();
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int nx = curr.first + dir[i][0];
+                int ny = curr.second + dir[i][1];
+                if (nx >= 0 && ny >= 0 && nx < rows && ny < cols) {
+                    if (dist[nx][ny] > dist[curr.first][curr.second] + 1) {
+                        dist[nx][ny] = dist[curr.first][curr.second] + 1;
+                        q.push({ nx, ny });
+                    }
+                }
+            }
+        }
+        return dist;
+    }
+};
