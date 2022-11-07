0ffe4481923a045f3ee4ea515569c7fa418dd8d5
Jazz <seunghohan25@gmail.com>
2022-11-08 00:45:36 +0900

    Time: 0 ms (100.00%), Space: 7.7 MB (77.56%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    int rob(vector<int>& nums) {
+        vector<int> memo(100, -1);
+        return robFrom(0, nums, memo);
+    }
+    
+    int robFrom(int i, vector<int>& nums, vector<int>& memo) {
+        if (i >= nums.size()) {
+            return 0;
+        }
+        if (memo[i] > -1) {
+            return memo[i];
+        }
+        
+        int ans = max(robFrom(i + 1, nums, memo), robFrom(i + 2, nums, memo) + nums[i]);
+        
+        // Cache for future use.
+        memo[i] = ans;
+        return ans;
+    }
+};
