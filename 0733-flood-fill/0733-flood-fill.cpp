fdeb6ffe995055b96c26aee780dbf4d721af9e0c
Jazz <seunghohan25@gmail.com>
2022-11-07 20:35:03 +0900

    Time: 30 ms (6.00%), Space: 14.1 MB (36.26%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,48 @@
+class Solution {
+
+public:
+    int m;
+    int n;
+    
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, 1, 0, -1};
+    
+    bool isIn(int x, int y) {
+        if (x < 0 || x >= m || y < 0 || y >= n) {
+            return false;
+        }
+        return true;
+    }
+    
+    void dfs(vector<vector<int>>& image, int x, int y, int color, int org, vector<vector<bool>>& visited) {
+        // base case
+        if (visited[x][y]) {
+            return;
+        }
+        
+        // cout << x << ", " << y << endl;
+    
+        visited[x][y] = true;
+        image[x][y] = color;
+        
+        for (int i = 0; i < 4; ++i) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (isIn(nx, ny) && image[nx][ny] == org) {
+                dfs(image, nx, ny, color, org, visited);
+            }
+        }
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        m = image.size();
+        n = image[0].size();
+        
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        
+        dfs(image, sr, sc, color, image[sr][sc], visited);
+        
+        
+        return image;
+    }
+};
