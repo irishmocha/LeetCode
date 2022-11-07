07933581e31425c7b8eb5920f9c0af63a4d44d4a
Jazz <seunghohan25@gmail.com>
2022-11-08 00:01:31 +0900

    Time: 11 ms (72.31%), Space: 13.1 MB (53.84%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, 1, 0, -1};
+    
     int orangesRotting(vector<vector<int>>& grid) {
-        vector<int> dir={-1,0,1,0,-1}; //used for finding all 4 adjacent coordinates
-        
         int m = grid.size();
         int n = grid[0].size();
         
         queue<pair<int, int>> q;
-        int fresh = 0; //To keep track of all fresh oranges left
+        int fresh = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] == 2) {
@@ -26,19 +27,26 @@ public:
                 pair<int, int> p = q.front();
                 q.pop();
                 for (int i = 0; i < 4; ++i) {
-                    int r = p.first + dir[i];
-                    int c = p.second + dir[i + 1];
-                    if(r>=0 && r<m && c>=0 && c<n &&grid[r][c]==1) {
+                    
+                    int r = p.first + dx[i];
+                    int c = p.second + dy[i];
+                    
+                    if(r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 1) {
                         grid[r][c] = 2;
                         q.push({r, c});
                         --fresh;
                     }
                 }
             }
-            ++minute; //incremented after each minute passes
+            ++minute;
         }
-        if (fresh > 0) return -1;
-        if (minute == -1) return 0;
+        
+        if (fresh > 0) {
+            return -1;
+        }
+        if (minute == -1) {
+            return 0;
+        }
         
         return minute;
         
