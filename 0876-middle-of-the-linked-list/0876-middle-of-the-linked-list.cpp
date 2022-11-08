933b4c28f60cb583fe725ba190adab09463f4b40
Jazz <seunghohan25@gmail.com>
2022-11-08 22:35:01 +0900

    Time: 5 ms (28.45%), Space: 7.1 MB (21.34%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -28,7 +28,7 @@ public:
         
         
         vector<ListNode*> A = {head};
-        while (A.back()->next != NULL) {
+        while (A.back()->next != nullptr) {
             A.push_back(A.back()->next);
         }
         return A[A.size() / 2];
