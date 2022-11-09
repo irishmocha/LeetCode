2ed91292b13186e8007008d0fd2df807a820e1c5
Jazz <seunghohan25@gmail.com>
2022-11-09 20:09:02 +0900

    Time: 6 ms (92.98%), Space: 7.3 MB (71.92%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -15,11 +15,10 @@ public:
             return true;
         }
         
+        
         for (int i = s1.size(); i < s2.size(); ++i) {
-            cout << freq2[s2[i] - 'a'] << endl;
-            ++freq2[s2[i] - 'a']; // sliding window에 들어갔으므로 사용 횟수 추가
-            cout << freq2[s2[i - s1.size()] - 'a'] << endl;
-            --freq2[s2[i - s1.size()] - 'a']; // sliding window에서 빠져나왔으므로 사용 횟수 -1
+            ++freq2[s2[i] - 'a'];
+            --freq2[s2[i - s1.size()] - 'a'];
             if (freq1 == freq2) {
                 return true;
             }
