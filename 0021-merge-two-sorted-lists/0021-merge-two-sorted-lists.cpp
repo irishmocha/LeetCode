d7c31b06f2afd853c3987a6af079dbe7cd0dc441
Jazz <seunghohan25@gmail.com>
2022-11-09 22:49:55 +0900

    Time: 18 ms (32.50%), Space: 14.9 MB (42.29%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -19,7 +19,7 @@ public:
             return list1;
         }
         
-        else if (list1->val < list2->val) {
+        if (list1->val < list2->val) {
             list1->next = mergeTwoLists(list1->next, list2);
             return list1;
         }
