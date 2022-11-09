d3cd20bbafece3d153f0ff71418c3224844c353e
Jazz <seunghohan25@gmail.com>
2022-11-09 22:32:15 +0900

    Time: 97 ms (86.46%), Space: 30.3 MB (56.15%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -5,20 +5,19 @@ public:
     int dx[4] = {-1, 0, 1, 0};
     int dy[4] = {0, 1, 0, -1};
     
-    bool isIn(int x, int y) {
-        if (x < 0 || x >= m || y < 0 || y >= n) {
-            return false;
+    bool isIn(int nx, int ny) {
+        if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
+            return true;
         }
-        return true;
+        return false;
     }
     
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         if (mat.size() == 0) {
             return mat;
         }
-        
-        int m = mat.size();
-        int n = mat[0].size();
+        m = mat.size();
+        n = mat[0].size();
         
         queue<pair<int, int>> q;
         vector<vector<int>> dist(m, vector<int> (n, INT_MAX));
@@ -32,21 +31,13 @@ public:
         }
 
         while (!q.empty()) {
-            auto curr = q.front();
-            q.pop();
+            auto curr = q.front();  q.pop();
             for (int i = 0; i < 4; i++) {
                 int nx = curr.first + dx[i];
                 int ny = curr.second + dy[i];
-                
-                if (
-                    (nx >= 0 && ny >= 0 && nx < m && ny < n) &&
-                   dist[nx][ny] > dist[curr.first][curr.second] + 1
-                   
-                   ) {
-                    // if (dist[nx][ny] > dist[curr.first][curr.second] + 1) {
-                        dist[nx][ny] = dist[curr.first][curr.second] + 1;
-                        q.push({nx, ny});
-                    // }
+                if (isIn(nx, ny) && dist[nx][ny] > dist[curr.first][curr.second] + 1) {
+                    dist[nx][ny] = dist[curr.first][curr.second] + 1;
+                    q.push({nx, ny});
                 }
             }
         }
