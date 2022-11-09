da71f20befcb175f5e4fba1ce90fbe68de191240
Jazz <seunghohan25@gmail.com>
2022-11-09 19:37:22 +0900

    Time: 20 ms (51.30%), Space: 7.4 MB (42.74%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int len1 = s1.size(), len2 = s2.size();
-        if(len1 > len2)
+        if (s1.size() > s2.size()) {
             return false;
-           
-        vector<int> freq1(26, 0), freq2(26, 0);
-
-        for(int i = 0; i < len1; i++){
-            freq1[s1[i]-'a']++; // s1에 있는 알파벳이 쓰인 횟수
-            freq2[s2[i]-'a']++; // sliding window - len1만큼 이동하면서 사용된 알파벳 체크
         }
         
-        // 두 vector가 같다는 뜻은 각 알파벳이 사용된 횟수가 같다는 뜻
-        if(freq1 == freq2)  return true;
+        vector<int> freq1(26, 0), freq2(26, 0);
+        for (int i = 0; i < s1.size(); ++i) {
+            ++freq1[s1[i] - 'a'];
+            ++freq2[s2[i] - 'a'];
+        }
+        if (freq1 == freq2) {
+            return true;
+        }
         
-        for(int i = len1; i < len2; i++){
-            freq2[s2[i]-'a']++; // sliding window에 들어갔으므로 사용 횟수 추가
-            freq2[s2[i-len1]-'a']--; // sliding window에서 빠져나왔으므로 사용 횟수 -1
-            if(freq1 == freq2)  return true;
+        for (int i = s1.size(); i < s2.size(); ++i) {
+            ++freq2[s2[i] - 'a'];
+            --freq2[s2[i - s1.size()] - 'a'];
+            if (freq1 == freq2) {
+                return true;
+            }
         }
         
         return false;
