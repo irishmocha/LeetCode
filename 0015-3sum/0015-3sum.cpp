1cb011fb594d95654c3eee3856ce5978987d4cfd
Jazz <seunghohan25@gmail.com>
2022-11-12 10:17:23 +0900

    Time: 196 ms (45.70%), Space: 24.6 MB (20.17%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,19 +7,23 @@ public:
         
         sort(nums.begin(), nums.end());
         
+        set<vector<int>> ts;
+        
         for (int i = 0; i < nums.size() - 2; ++i) {
-            if (i != 0 && nums[i] == nums[i - 1])
+            if (i != 0 && nums[i] == nums[i - 1]) {
                 continue;
+            }
             
             int lower = i + 1;
             int upper = nums.size() - 1;
             while (lower < upper) {
                 int sum = nums[i] + nums[lower] + nums[upper];
                 if (sum == 0) {
-                    ans.push_back({nums[i] , nums[lower] , nums[upper]});
+                    ts.insert({nums[i] , nums[lower] , nums[upper]});
+                    // ans.push_back({nums[i] , nums[lower] , nums[upper]});    // 1
                     ++lower; --upper;
-                    while (nums[lower] == nums[lower - 1] && lower < upper)
-                        ++lower;
+                    // while (nums[lower] == nums[lower - 1] && lower < upper)  // 2-1
+                    //     ++lower;                                                 2-2
                     // while (nums[lower] == nums[upper + 1] && lower < upper)
                     //     --upper;
                 }
@@ -34,6 +38,8 @@ public:
             }
         }
         
-        return ans;
+        vector<vector<int>> answer(ts.begin(), ts.end());
+        
+        return answer;
     }
 };
