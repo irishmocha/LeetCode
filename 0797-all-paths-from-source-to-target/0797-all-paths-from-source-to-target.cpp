b6d4c7edddb166e24825ff7a353699fab9abbe71
Jazz <seunghohan25@gmail.com>
2022-11-12 17:39:20 +0900

    Time: 40 ms (33.29%), Space: 17.9 MB (12.39%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector <vector<int>> result;
+        int n=graph.size(), s=0, e=n;
+        vector <int> adj[n];
+        for(int i=0; i<n; i++)
+            for(auto g : graph[i])
+                adj[i].push_back(g);
+        
+        queue <vector<int>> q;
+        vector <int> path;
+        q.push({0});
+        while(!q.empty()){
+            path = q.front();
+            q.pop();
+            if(path.back() == n-1)
+                result.push_back(path);
+            
+            for(auto ele : adj[path.back()]){
+                vector <int> temp = path;
+                temp.push_back(ele);
+                q.push(temp);
+            }
+        }
+        return  result;
+    }
+};
