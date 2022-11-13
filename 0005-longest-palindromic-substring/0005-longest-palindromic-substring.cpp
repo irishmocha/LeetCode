00b4561029acabab6d4856c457c2e174469f866f
Jazz <seunghohan25@gmail.com>
2022-11-13 13:17:19 +0900

    Time: 310 ms (31.22%), Space: 35 MB (31.22%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.length();
+        // int n = s.length();
         string ans = "";
-        if (n == 0)
+        if (s.length() == 0)
             return ans;
         
-        vector<vector<bool>> dp(n, vector<bool>(n));
+        vector<vector<bool>> dp(s.length(), vector<bool>(s.length()));
         
         //Every Single character is palindrome
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < s.length(); i++) {
             dp[i][i] = true;
         }
 
         // string ans = "";
         ans += s[0];
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i + 1; j < n; j++) {
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i + 1; j < s.length(); j++) {
                 if (s[i] == s[j]) {
                     //If it is of two character OR if its susbtring is palindrome.
                     if (j - i == 1 || dp[i + 1][j - 1]) {
