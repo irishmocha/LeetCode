8bd2d7dee6fe0f0b8c3d4105f01fc415e090d12d
Jazz <seunghohan25@gmail.com>
2022-11-13 12:41:23 +0900

    Time: 0 ms (100.00%), Space: 8 MB (19.02%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -5,23 +5,23 @@ public:
             return nums[0];
         }
         
-        vector<int> dp(nums.size(), 0);
+        vector<int> dp1(nums.size(), 0);
         vector<int> dp2(nums.size(), 0);
         
-        dp[0] = nums[0];
-        dp[1] = nums[0];
+        dp1[0] = nums[0];
+        dp1[1] = nums[0];
         
         dp2[0] = 0;
         dp2[1] = nums[1];
         
         for (size_t i = 2;i < nums.size(); ++i) {
             if (i != nums.size() - 1) {
-                dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+                dp1[i] = max(dp1[i - 2] + nums[i], dp1[i - 1]);
             }
             dp2[i] = max(dp2[i - 2] + nums[i], dp2[i - 1]);
         }
         
-        int result = max(dp[nums.size() - 2], dp2[nums.size() - 1]);
+        int result = max(dp1[nums.size() - 2], dp2[nums.size() - 1]);
         
         return result;
     }
