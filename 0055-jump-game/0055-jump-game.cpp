010c75ce0f98a101f6ef2202b9dfaaae4f224942
Jazz <seunghohan25@gmail.com>
2022-11-13 12:52:14 +0900

    Time: 79 ms (87.69%), Space: 48.5 MB (46.20%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int minjump = 0;
-        for (int i = nums.size() - 2; i >= 0; --i) {
-            ++minjump;
-            if (nums[i] >= minjump) {
-			    minjump = 0;
-            }
-        }
+        int n=nums.size();
+        int reachable=0;
         
-        return minjump == 0;
+        for(int i=0;i<n;i++){
+            if(i>reachable) return false;
+            reachable=max(reachable,i+nums[i]);
+        }
+        return true;
     }
 };
