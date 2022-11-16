720a81d5b3ec1a3179a5c8eaa5689a93083710f4
Jazz <seunghohan25@gmail.com>
2022-11-16 11:22:31 +0900

    Time: 0 ms (100.00%), Space: 7.9 MB (25.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    int rec(vector<int>& nums, int idx, vector<int>& dp) {
-        if (idx >= nums.size()) {
-            return 0;
-        }
-        if (dp[idx] != -1) {
-            return dp[idx];
-        }
+//     int rec(vector<int>& nums, int i, vector<int>& dp) {
+//         if (i >= nums.size()) {
+//             return 0;
+//         }
+//         if (dp[i] != -1) {
+//             return dp[i];
+//         }
         
-        return dp[idx] = max(rec(nums, idx + 1, dp), nums[idx] + rec(nums, idx + 2, dp));
-    }
+//         return dp[i] = max(rec(nums, i + 1, dp), nums[i] + rec(nums, i + 2, dp));
+//     }
 
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size() + 1, -1);
+        // vector<int> dp(nums.size() + 1, -1);
         
-        return rec(nums, 0, dp);
+        // return rec(nums, 0, dp);
       
       
-      /*
+      
         if (nums.size() == 1)
             return nums[0];
         
-        vector<int>dp(nums.size());
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
+        vector<int> dp(nums.size());
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
         
         for (int i = 2; i < nums.size(); ++i) {
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
         }
         
         return dp[nums.size()-1];
-        */
+        
   }
 };
