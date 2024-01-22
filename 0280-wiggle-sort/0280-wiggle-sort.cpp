cd4aa4224bb9d80cb678fe413bc3036234ecf9fb
irishmocha <kyrix.gh@gmail.com>
2024-01-22 21:40:50 +0900

    Update 0280-wiggle-sort.cpp

--- 0280-wiggle-sort/0280-wiggle-sort.cpp
+++ 0280-wiggle-sort/0280-wiggle-sort.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        bool inc = true;
-        for (int i = 0; i < nums.size() - 1; ++i) {
-            if (inc != (nums[i] <= nums[i + 1])) {
-                std::swap(nums[i], nums[i + 1]);
-            }
-            inc = !inc;
-        }
+        for (int i = 1; i < nums.size(); ++i)
+            if (i % 2 && nums[i - 1] > nums[i] || i % 2 == 0 && nums[i - 1] < nums[i])
+                swap(nums[i - 1], nums[i]);
     }
 };
